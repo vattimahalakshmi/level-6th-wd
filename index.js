b2152cf9cfc5af70309d654e7842d0385f730a77
MahalakshmiVatti <vatti.mahalakshmi14@gmail.com>
2023-01-05 22:27:48 +0530

    L9 commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require("./app");
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Started express server at port 3000");
+
+app.listen(3000, () => {
+  console.log("Started  the express server at port 3000");
 });
